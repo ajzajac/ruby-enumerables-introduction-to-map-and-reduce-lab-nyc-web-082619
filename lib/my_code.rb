66a716fd77a69b81f89315ec45edb2c79c240b12
Andrew Zajac <ajzajac@icloud.com>
2019-08-12 17:52:57 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -49,8 +49,8 @@ def reduce_to_all_true(source_array)
   while i < source_array.length do
     if source_array[i] == false 
       false
-      i += 1
     end
+      i += 1
     else 
       true 
 end
